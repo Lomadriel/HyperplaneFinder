fff8c3531107f13887143de8979fb07bf18465c9
Maxime Pinard <maxime.pin@live.fr>
2018-04-03 17:56:16 +0200

    Added JSON library

---
+++ cmake/json.cmake
@@ -0,0 +1,10 @@
+message(STATUS "Configuring JSON")
+
+# Config
+set(JSON_NAME      "json")
+set(JSON_VERSION   "3.1.2")
+
+get_filename_component(JSON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/extlibs/${JSON_NAME}-${JSON_VERSION}/include REALPATH)
+
+# Message
+message("> include: ${JSON_INCLUDE_DIR}")
