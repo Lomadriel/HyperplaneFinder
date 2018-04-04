5d633808bf5fc74777826951a52ee36ee2a9e414
Maxime Pinard <maxime.pin@live.fr>
2018-04-04 14:52:24 +0200

    Updated INJA library

--- cmake/inja.cmake
+++ cmake/inja.cmake
@@ -2,7 +2,7 @@ message(STATUS "Configuring INJA")
 
 # Config
 set(INJA_NAME      "inja")
-set(INJA_VERSION   "1.0.0")
+set(INJA_VERSION   "git-1cb6b15")
 
 get_filename_component(INJA_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/extlibs/${INJA_NAME}-${INJA_VERSION}/include REALPATH)
 
