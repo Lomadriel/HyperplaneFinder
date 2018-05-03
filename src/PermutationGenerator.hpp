29a571731c9ed7d2f2be70e5bdfdb9af187da372
Maxime Pinard <maxime.pin@live.fr>
2018-05-03 20:44:06 +0200

    Added MultiPermutationGenerator

--- src/PermutationGenerator.hpp
+++ src/PermutationGenerator.hpp
@@ -4,19 +4,19 @@
 
 #include <array>
 
-namespace{
+namespace {
 
 	template<size_t nbrElements>
-	std::array<unsigned int, nbrElements> generateInitialPermutation(){
+	std::array<unsigned int, nbrElements> generateInitialPermutation() {
 		std::array<unsigned int, nbrElements> initial_permutation;
-		for(unsigned int i = 0; i < nbrElements; ++i){
+		for(unsigned int i = 0; i < nbrElements; ++i) {
 			initial_permutation[i] = i;
 		}
 		return initial_permutation;
 	}
 
 	template<size_t nbrElements>
-	std::array<unsigned int, nbrElements - 1> withoutFirst(const std::array<unsigned int, nbrElements>& in_values){
+	std::array<unsigned int, nbrElements - 1> withoutFirst(const std::array<unsigned int, nbrElements>& in_values) {
 		std::array<unsigned int, nbrElements - 1> out_values;
 		std::copy(in_values.cbegin() + 1, in_values.cend(), out_values.begin());
 		return out_values;
@@ -26,16 +26,27 @@ namespace{
 	struct Permutation;
 
 	template<>
-	struct Permutation<2>{
+	struct Permutation<2> {
 
-		Permutation(const std::array<unsigned int, 2>& values) noexcept
+		explicit Permutation(const std::array<unsigned int, 2>& values) noexcept
 		  : permutation(values)
-		  , ended(false) {
+		    , ended(false) {
 
 		}
 
+		Permutation(const Permutation<2>&) = delete;
+		Permutation<2>& operator=(const Permutation<2>&) = delete;
+
+		Permutation(Permutation<2>&&) noexcept = default;
+		Permutation<2>& operator=(Permutation<2>&&) noexcept = default;
+
+		void reset(const std::array<unsigned int, 2>& values) {
+			permutation = values;
+			ended = false;
+		}
+
 		bool nextPermutation() {
-			if(!ended){
+			if(!ended) {
 				std::swap(permutation[0], permutation[1]);
 				ended = true;
 				return true;
@@ -48,9 +59,9 @@ namespace{
 	};
 
 	template<size_t permutationSize>
-	struct Permutation{
+	struct Permutation {
 
-		Permutation(const std::array<unsigned int, permutationSize>& values) noexcept
+		explicit Permutation(const std::array<unsigned int, permutationSize>& values) noexcept
 		  : subPermutation(withoutFirst(values))
 		  , permutation(values)
 		  , current(values[0])
@@ -58,14 +69,27 @@ namespace{
 
 		}
 
+		Permutation(const Permutation<permutationSize>&) = delete;
+		Permutation<permutationSize>& operator=(const Permutation<permutationSize>&) = delete;
+
+		Permutation(Permutation<permutationSize>&&) noexcept = default;
+		Permutation<permutationSize>& operator=(Permutation<permutationSize>&&) noexcept = default;
+
+		void reset(const std::array<unsigned int, permutationSize>& values) {
+			subPermutation.reset(withoutFirst(values));
+			permutation = values;
+			current = values[0];
+			position = 0;
+		}
+
 		bool nextPermutation() {
 			++position;
-			if(position < permutationSize){
+			if(position < permutationSize) {
 				std::swap(permutation[position - 1], permutation[position]);
 				return true;
 			}
 
-			if(!subPermutation.nextPermutation()){
+			if(!subPermutation.nextPermutation()) {
 				return false;
 			}
 
@@ -77,13 +101,13 @@ namespace{
 
 		Permutation<permutationSize - 1> subPermutation;
 		std::array<unsigned int, permutationSize> permutation;
-		const unsigned int current;
+		unsigned int current;
 		unsigned int position;
 	};
 
 }
 
-namespace segre{
+namespace segre {
 
 	template<size_t nbrElements>
 	class PermutationGenerator {
@@ -96,13 +120,19 @@ namespace segre{
 
 		}
 
+		PermutationGenerator(const PermutationGenerator<nbrElements>&) = delete;
+		PermutationGenerator<nbrElements>& operator=(const PermutationGenerator<nbrElements>&) = delete;
+
+		PermutationGenerator(PermutationGenerator<nbrElements>&&) noexcept = default;
+		PermutationGenerator<nbrElements>& operator=(PermutationGenerator<nbrElements>&&) noexcept = default;
+
 		void reset() {
-			m_current_permutation = generateInitialPermutation<nbrElements>();
+			m_current_permutation.reset(generateInitialPermutation<nbrElements>());
 			m_finished = false;
 		}
 
 		std::array<unsigned int, nbrElements> nextPermutation() {
-			std::array<unsigned int, nbrElements> permutation = m_current_permutation.permutation;
+			const std::array<unsigned int, nbrElements> permutation = m_current_permutation.permutation;
 			m_finished = !m_current_permutation.nextPermutation();
 			return permutation;
 		}
@@ -114,6 +144,68 @@ namespace segre{
 	private:
 
 		Permutation<nbrElements> m_current_permutation;
+		bool m_finished;
+	};
+
+	template<size_t... nbrElements>
+	class MultiPermutationGenerator {
+
+	public:
+
+		MultiPermutationGenerator() noexcept
+		  : m_generators{}
+		  , m_current_multiperm{}
+		  , m_finished{false} {
+
+			apply([](auto& perm, auto& generator) {
+				perm = generator.nextPermutation();
+			});
+		}
+
+		MultiPermutationGenerator(const MultiPermutationGenerator<nbrElements...>&) = delete;
+		MultiPermutationGenerator<nbrElements...>& operator=(const MultiPermutationGenerator<nbrElements...>&) = delete;
+
+		MultiPermutationGenerator(MultiPermutationGenerator<nbrElements...>&&) noexcept = default;
+		MultiPermutationGenerator<nbrElements...>& operator=(MultiPermutationGenerator<nbrElements...>&&) noexcept = default;
+
+		std::tuple<std::array<unsigned int, nbrElements>...> nextPermutation() {
+			const std::tuple<std::array<unsigned int, nbrElements>...> permutations = m_current_multiperm;
+			unsigned int i = 0;
+			unsigned int to_increment = 0;
+			apply([&](auto& perm, auto& generator) {
+				if(i == to_increment) {
+					if(generator.isFinished()) {
+						if(i == sizeof...(nbrElements) - 1) {
+							m_finished = true;
+						}
+						++to_increment;
+						generator.reset();
+					}
+					perm = generator.nextPermutation();
+				}
+				++i;
+			});
+			return permutations;
+		}
+
+		bool isFinished() const {
+			return m_finished;
+		}
+
+	private:
+
+		template<typename Func>
+		void apply(Func func) {
+			apply_impl(func, std::make_index_sequence<sizeof...(nbrElements)>());
+		}
+
+		template<typename Func, size_t... Is>
+		void apply_impl(Func func, std::index_sequence<Is...>) {
+			(func(std::get<Is>(m_current_multiperm), std::get<Is>(m_generators)), ...);
+		}
+
+		std::tuple<PermutationGenerator<nbrElements>...> m_generators;
+		std::tuple<std::array<unsigned int, nbrElements>...> m_current_multiperm;
 		bool m_finished;
 	};
 
