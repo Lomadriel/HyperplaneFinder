f62631f8a8c3bcb5f93005e099d0b5e514e17121
Maxime Pinard <maxime.pin@live.fr>
2018-05-06 01:21:43 +0200

    Cmake use absolute path rather than realpath for dependencies

--- cmake/inja.cmake
+++ cmake/inja.cmake
@@ -4,7 +4,7 @@ message(STATUS "Configuring INJA")
 set(INJA_NAME      "inja")
 set(INJA_VERSION   "git-1cb6b15")
 
-get_filename_component(INJA_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/extlibs/${INJA_NAME}-${INJA_VERSION}/include REALPATH)
+get_filename_component(INJA_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/extlibs/${INJA_NAME}-${INJA_VERSION}/include ABSOLUTE)
 
 # Message
 message("> include: ${INJA_INCLUDE_DIR}")
--- cmake/json.cmake
+++ cmake/json.cmake
@@ -4,7 +4,7 @@ message(STATUS "Configuring JSON")
 set(JSON_NAME      "json")
 set(JSON_VERSION   "3.1.2")
 
-get_filename_component(JSON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/extlibs/${JSON_NAME}-${JSON_VERSION}/include REALPATH)
+get_filename_component(JSON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/extlibs/${JSON_NAME}-${JSON_VERSION}/include ABSOLUTE)
 
 # Message
 message("> include: ${JSON_INCLUDE_DIR}")
