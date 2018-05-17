6472774cb0a5b8357e81a4d34b10524fb3f3c2e8
Maxime Pinard <maxime.pin@live.fr>
2018-05-18 00:23:08 +0200

    Added separateByPermutations for one lines table entry

--- src/VeldkampLinesUtility.hpp
+++ src/VeldkampLinesUtility.hpp
@@ -16,6 +16,12 @@ namespace segre{
 	  const std::vector<std::vector<unsigned int>>& hyp_permutations_table,
 	  size_t permutation_number
 	);
+
+	template<size_t Dimension, size_t NbrPointsPerLine, size_t NbrPoints = math::pow(NbrPointsPerLine, Dimension)>
+	std::vector<VeldkampLineTableEntry> separateByPermutations(
+	  const VeldkampLineTableEntryWithLines<Dimension, NbrPointsPerLine>& lines_table_entry,
+	  const std::vector<std::vector<unsigned int>>& hyp_permutations_table
+	);
 }
 
 // Implementations
@@ -28,6 +34,46 @@ namespace segre {
 			permuted_line[i] = hyp_permutations_table[line[i]][permutation_number];
 		}
 		return permuted_line;
+	}
+
+	template<size_t Dimension, size_t NbrPointsPerLine, size_t NbrPoints>
+	std::vector<VeldkampLineTableEntry> separateByPermutations(const VeldkampLineTableEntryWithLines <Dimension, NbrPointsPerLine>& lines_table_entry, const std::vector<std::vector<unsigned int>>& hyp_permutations_table) {
+		std::vector<VeldkampLineTableEntry> output_table;
+		std::vector<std::array<unsigned int, NbrPointsPerLine>> sorted_lines(lines_table_entry.lines);
+		std::for_each(sorted_lines.begin(), sorted_lines.end(), [](std::array<unsigned int, NbrPointsPerLine>& line){std::sort(line.begin(), line.end());});
+
+		std::vector<bool> flags;
+		flags.reserve(lines_table_entry.lines.size());
+		for(size_t i = 0; i < lines_table_entry.lines.size(); ++i){
+			flags.push_back(false);
+		}
+		while(true){
+			const size_t pos = static_cast<const size_t>(std::find(flags.cbegin(), flags.cend(), false) - flags.cbegin());
+			if(pos >= flags.size()) {
+				break;
+			}
+			const std::array<unsigned int, NbrPointsPerLine>& line = lines_table_entry.lines[pos];
+			flags[pos] = true;
+			VeldkampLineTableEntry table_entry = lines_table_entry.entry;
+			table_entry.count = 1;
+			for(size_t i = 0; i < decltype(makeMultiPermutationsGenerator<Dimension, NbrPointsPerLine>())::getPermutationsNumber(); ++i){
+				std::array<unsigned int, NbrPointsPerLine> permuted_line = getPermutation<Dimension, NbrPointsPerLine>(line, hyp_permutations_table, i);
+				std::sort(permuted_line.begin(), permuted_line.end());
+				const size_t pos2 = static_cast<const size_t>(std::find(sorted_lines.cbegin(), sorted_lines.cend(), permuted_line) - sorted_lines.cbegin());
+				if(pos2 < sorted_lines.size()) {
+					if(!flags[pos2]){
+						flags[pos2] = true;
+						++table_entry.count;
+					}
+				}
+				else{
+					assert(false && "impossible");
+				}
+			}
+			output_table.push_back(std::move(table_entry));
+		}
+
+		return output_table;
 	}
 }
 
