6b73c61c873cd5064523e622a8d0932a825ec835
Maxime Pinard <maxime.pin@live.fr>
2018-06-02 15:13:07 +0200

    Documented HyperplanesUtility

--- src/HyperplanesUtility.hpp
+++ src/HyperplanesUtility.hpp
@@ -14,40 +14,171 @@
 // Declarations
 namespace segre {
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Convert an hyperplane representation from a std::bitset to a
+	 *             std::vector.
+	 *
+	 * @details    In the std::bitset representation, each bit represent a point
+	 *             of the hyperplane, included if equal true, excluded if false.
+	 *
+	 *             In the std::vector representation, the vector contain the
+	 *             index of all included points in increasing order.
+	 *
+	 * @param[in]  hyperplane  The hyperplane to convert
+	 *
+	 * @tparam     NbrPoints   Number of points of the geometry
+	 *
+	 * @return     The hyperplane converted to std::vector
+	 */
 	template<size_t NbrPoints>
 	std::vector<unsigned int> bitsetToVector(const std::bitset<NbrPoints>& hyperplane);
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Convert an hyperplane representation from a std::vector to a
+	 *             std::bitset.
+	 *
+	 * @details    In the std::vector representation, the vector contain the
+	 *             index of all included points in increasing order.
+	 *
+	 *             In the std::bitset representation, each bit represent a point
+	 *             of the hyperplane, included if equal true, excluded if false.
+	 *
+	 * @param[in]  hyperplane  The hyperplane to convert
+	 *
+	 * @tparam     NbrPoints   Number of points of the geometry
+	 *
+	 * @return     The hyperplane converted to std::bitset
+	 */
 	template<size_t NbrPoints>
 	std::bitset<NbrPoints> vectorToBitset(const std::vector<unsigned int>& hyperplane);
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Utility struct for passing number from
+	 *             makeMultiPermutationsGenerator() to
+	 *             makeMultiPermutationsGenerator_impl().
+	 *
+	 * @tparam     N     Number to pass
+	 */
 	template<size_t N>
 	struct nothing {
 	};
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Makes a multi permutations generator with @p Dimension times
+	 *             a permutation of @p NbrPointsPerLine elements and one time a
+	 *             permutation of @p Dimension elements.
+	 *
+	 * @details    The multi permutations generator returned is adapted for
+	 *             generating all permutations of an hyperplane of a geometry of
+	 *             dimension @p Dimension and with @p NbrPointsPerLine points
+	 *             per lines.
+	 *
+	 * @tparam     Dimension         Dimension of the geometry
+	 * @tparam     NbrPointsPerLine  Number of points per lines of the geometry
+	 *
+	 * @return     A multi permutations generator
+	 */
 	template<size_t Dimension, size_t NbrPointsPerLine>
 	auto makeMultiPermutationsGenerator();
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Implementation of makeMultiPermutationsGenerator(), needed
+	 *             to generate an index_repetition
+	 */
 	template<size_t Dimension, size_t... R>
 	MultiPermutationGenerator<R..., Dimension> makeMultiPermutationsGenerator_impl(nothing<Dimension>, index_repetition<R...>);
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Call @p func on all elements of @p tuple in the increasing
+	 *             order.
+	 *
+	 * @param[in]  func   The function / lambda to call
+	 * @param[in]  tuple  The tuple to iterate over
+	 *
+	 * @tparam     Func   Type of @p func
+	 * @tparam     T      Types contained by the tuple
+	 */
 	template<typename Func, typename... T>
 	void iterateOnTuple(Func func, const std::tuple<T...>& tuple);
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Implementation of iterateOnTuple(), needed to generate an
+	 *             std::index_sequence
+	 */
 	template<typename Func, typename... T, size_t... Is>
 	void iterateOnTuple_impl(Func func, const std::tuple<T...>& tuple, std::index_sequence<Is...>);
 
-	template<size_t Dimension, size_t NbrPointsPerLine, typename Perm>
-	std::tuple<std::array<std::array<unsigned int, NbrPointsPerLine>, Dimension>, std::array<unsigned int, Dimension>> convertPermutation(const Perm& permutation);
-
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Convert a permutation from a tuple of [@p Dimension times a
+	 *             permutation of @p NbrPointsPerLine elements and one time a
+	 *             permutation of @p Dimension elements] to a tuple of [an array
+	 *             with @p Dimension permutations of @p NbrPointsPerLine
+	 *             elements] and [a permutation of @p Dimension elements]
+	 *
+	 * @param[in]  permutation       The permutation to convert
+	 *
+	 * @tparam     Dimension         Dimension of the geometry
+	 * @tparam     NbrPointsPerLine  Number of points per lines of the geometry
+	 * @tparam     Permutation       Type of @p permutation
+	 *
+	 * @return     The permutation converted
+	 */
 	template<size_t Dimension, size_t NbrPointsPerLine, typename Permutation>
-	std::vector<unsigned int> applyPermutation(const std::vector<unsigned int>& points, const Permutation& permutation);
+	std::tuple<std::array<std::array<unsigned int, NbrPointsPerLine>, Dimension>, std::array<unsigned int, Dimension>> convertPermutation(const Permutation& permutation);
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Apply a permutation to an hyperplane.
+	 *
+	 * @param[in]  hyperplane        The hyperplane to permute
+	 * @param[in]  permutation       The permutation to apply
+	 *
+	 * @tparam     Dimension         Dimension of the geometry
+	 * @tparam     NbrPointsPerLine  Number of points per lines of the geometry
+	 * @tparam     Permutation       Type of @p permutation
+	 *
+	 * @return     The permuted hyperplane
+	 */
+	template<size_t Dimension, size_t NbrPointsPerLine, typename Permutation>
+	std::vector<unsigned int> applyPermutation(const std::vector<unsigned int>& hyperplane, const Permutation& permutation);
+
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Calculates the hyperplane stabilisation permutations.
+	 *
+	 * @details    The hyperplane stabilisation permutations are the
+	 *             permutations that doesn't modify the hyperplane, the
+	 *             hyperplane stay the same on permutation application.
+	 *
+	 * @param[in]  hyperplane        The hyperplane
+	 *
+	 * @tparam     Dimension         Dimension of the geometry
+	 * @tparam     NbrPointsPerLine  Number of points per lines of the geometry
+	 * @tparam     NbrPoints         Number of points of the geometry
+	 *
+	 * @return     The hyperplane stabilisation permutations.
+	 */
 	template<size_t Dimension, size_t NbrPointsPerLine, size_t NbrPoints = math::pow(NbrPointsPerLine, Dimension)>
 	std::vector<std::tuple<std::array<std::array<unsigned int, NbrPointsPerLine>, Dimension>, std::array<unsigned int, Dimension>>> computeHyperplaneStabilisationPermutations(std::bitset<NbrPoints> hyperplane);
 
+	/*------------------------------------------------------------------------*//**
+	 * @brief      Makes the permutations table, the table of the result of
+	 *             applying all possible permutation on each hyperplane of @p
+	 *             hyperplanes.
+	 *
+	 * @details    The element at position (@c n, @c m) is the number of the
+	 *             hyperplane from the @p hyperplanes that is the result of
+	 *             applying permutation number @c m on hyperplane number @c n
+	 *             from the @p hyperplanes.
+	 *
+	 * @param[in]  hyperplanes       The hyperplanes to generate the table
+	 *
+	 * @tparam     Dimension         Dimension of the geometry
+	 * @tparam     NbrPointsPerLine  Number of points per lines of the geometry
+	 * @tparam     NbrPoints         Number of points of the geometry
+	 *
+	 * @return     The permutations table
+	 */
 	template<size_t Dimension, size_t NbrPointsPerLine, size_t NbrPoints = math::pow(NbrPointsPerLine, Dimension)>
 	std::vector<std::vector<unsigned int>> makePermutationsTable(const std::vector<std::bitset<NbrPoints>>& hyperplanes);
-
 }
 
 // Implementations
@@ -94,8 +225,8 @@ namespace segre {
 		(func(std::get<Is>(tuple)), ...);
 	}
 
-	template<size_t Dimension, size_t NbrPointsPerLine, typename Perm>
-	std::tuple<std::array<std::array<unsigned int, NbrPointsPerLine>, Dimension>, std::array<unsigned int, Dimension>> convertPermutation(const Perm& permutation) {
+	template<size_t Dimension, size_t NbrPointsPerLine, typename Permutation>
+	std::tuple<std::array<std::array<unsigned int, NbrPointsPerLine>, Dimension>, std::array<unsigned int, Dimension>> convertPermutation(const Permutation& permutation) {
 		std::tuple<std::array<std::array<unsigned int, NbrPointsPerLine>, Dimension>, std::array<unsigned int, Dimension>> permutation_value;
 		size_t i = 0;
 		iterateOnTuple([&](const auto& sub_permutation) {
@@ -117,10 +248,10 @@ namespace segre {
 	}
 
 	template<size_t Dimension, size_t NbrPointsPerLine, typename Permutation>
-	std::vector<unsigned int> applyPermutation(const std::vector<unsigned int>& points, const Permutation& permutation) {
-		std::vector<unsigned int> permuted_points;
-		permuted_points.reserve(points.size());
-		for(unsigned int point : points) {
+	std::vector<unsigned int> applyPermutation(const std::vector<unsigned int>& hyperplane, const Permutation& permutation) {
+		std::vector<unsigned int> permuted_hyperplane;
+		permuted_hyperplane.reserve(hyperplane.size());
+		for(unsigned int point : hyperplane) {
 			std::array<unsigned int, Dimension> permuted_point_coords;
 			unsigned int i = 0;
 			iterateOnTuple([&](const auto& sub_permutation) {
@@ -141,10 +272,10 @@ namespace segre {
 			for(unsigned int j = 0; j < Dimension; ++j) {
 				permuted_point += permuted_point_coords[j] * math::pow(static_cast<unsigned int>(NbrPointsPerLine), j);
 			}
-			permuted_points.push_back(permuted_point);
+			permuted_hyperplane.push_back(permuted_point);
 		}
-		std::sort(permuted_points.begin(), permuted_points.end());
-		return permuted_points;
+		std::sort(permuted_hyperplane.begin(), permuted_hyperplane.end());
+		return permuted_hyperplane;
 	}
 
 	template<size_t Dimension, size_t NbrPointsPerLine, size_t NbrPoints>
