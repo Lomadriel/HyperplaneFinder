c35899c77123f727aab32d4443e030ae4da2e463
Maxime Pinard <maxime.pin@live.fr>
2018-09-09 21:21:43 -0400

    Brute force dimension 3 c-hyperplanes

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 using json = nlohmann::json;
 
 constexpr size_t PPL = 4; // Points Per Lines
-constexpr bool COMPUTE_AND_PRINT_POINTS_ORDER = true;
+constexpr bool COMPUTE_AND_PRINT_POINTS_ORDER = false;
 constexpr bool PRINT_SUBGEOMETRIES = true;
 
 template<int N>
@@ -24,6 +24,51 @@ using VPoints = std::vector<std::bitset<math::pow(PPL,N)>>;
 
 template<int>
 using VLines = segre::VeldkampLines<PPL>;
+
+VPoints<3> bruteForceD3Hyperplanes(const segre::PointGeometry<3, PPL, 48>& geometry3, const VPoints<2>& vPoints2){
+	constexpr auto NbrPointsD3 = math::pow(PPL,3);
+	constexpr auto NbrPointsD2 = math::pow(PPL,2);
+	VPoints<3> vPoints3;
+
+	for (const auto& vPoint : vPoints2) {
+		std::bitset<NbrPointsD3> newH;
+
+		newH |= segre::copyBitset<NbrPointsD3, NbrPointsD2>(vPoint);
+		newH |= segre::copyBitset<NbrPointsD3, NbrPointsD2>(vPoint) << NbrPointsD2;
+		newH |= segre::copyBitset<NbrPointsD3, NbrPointsD2>(vPoint) << (2 * NbrPointsD2);
+		newH |= segre::copyBitset<NbrPointsD3, NbrPointsD2>(~vPoint) << (3 * NbrPointsD2);
+
+		if (geometry3.isHyperplane(newH)) {
+			vPoints3.push_back(newH);
+		}
+	}
+
+	CombinationGenerator cg;
+	cg.initialize(static_cast<unsigned int>(vPoints2.size()), PPL);
+
+	while (!cg.isFinished()) {
+
+		std::bitset<NbrPointsD3> newH;
+
+		{
+			unsigned int i = 0;
+			for (unsigned int combination : cg.nextCombination()) {
+				newH |= segre::copyBitset<NbrPointsD3, NbrPointsD2>(vPoints2[combination]) << (i * NbrPointsD2);
+				++i;
+			}
+
+			if (geometry3.isHyperplane(newH)) {
+				vPoints3.push_back(newH);
+			}
+		}
+
+		if (cg.getNumLeft() % 100'000'000 == 0) {
+			std::cout << cg.getNumLeft() << '\n';
+		}
+	}
+
+	return vPoints3;
+}
 
 int main() {
 	const auto time_start = std::chrono::system_clock::now();
@@ -37,47 +82,26 @@ int main() {
 	segre::PointGeometry<4, PPL, 256> geometry4(geometry3.computeCartesianProduct(), geometry3.buildTensorPoints());
 
 	VPoints<2> vPoints2 = geometry2.findHyperplanesByBruteforce(); // brut force
-	VLines<2> vLines2 = geometry2.computeVeldkampLines(vPoints2);
-	geometry2.distinguishVeldkampLines(vLines2, vPoints2, geometry3);
 
 	std::vector<segre::HyperplaneTableEntry> geometry2_hyp_table = geometry2.makeHyperplaneTable<COMPUTE_AND_PRINT_POINTS_ORDER>(vPoints2);
 	std::sort(geometry2_hyp_table.begin(), geometry2_hyp_table.end(), [] (const segre::HyperplaneTableEntry& a, const segre::HyperplaneTableEntry& b) {
 		return a.nbrPoints > b.nbrPoints;
 	});
-	std::vector<segre::VeldkampLineTableEntry> geometry2_lin_table = geometry2.makeVeldkampLinesTable(vLines2, vPoints2, geometry2_hyp_table);
-	std::sort(geometry2_lin_table.begin(), geometry2_lin_table.end(), [](const segre::VeldkampLineTableEntry& a, const segre::VeldkampLineTableEntry& b){
-		return std::make_tuple(a.isProjective, a.coreNbrPoints, a.coreNbrLines) < std::make_tuple(b.isProjective, b.coreNbrPoints, b.coreNbrLines);
-	});
 
-	VPoints<3> vPoints3 = geometry2.computeHyperplanesFromVeldkampLines(vPoints2, vLines2.projectives);
-	VLines<3> vLines3 = geometry3.computeVeldkampLines(vPoints3);
-	geometry3.distinguishVeldkampLines(vLines3, vPoints3, geometry4);
+	VPoints<3> vPoints3 = bruteForceD3Hyperplanes(geometry3, vPoints2);
 
-	std::vector<segre::HyperplaneTableEntry> geometry3_hyp_table = geometry3.makeHyperplaneTable<COMPUTE_AND_PRINT_POINTS_ORDER>(vPoints3, geometry2_hyp_table);
+	std::vector<segre::HyperplaneTableEntry> geometry3_hyp_table = geometry3.makeHyperplaneTable<COMPUTE_AND_PRINT_POINTS_ORDER>(vPoints3);
 	std::sort(geometry3_hyp_table.begin(), geometry3_hyp_table.end(), [] (const segre::HyperplaneTableEntry& a, const segre::HyperplaneTableEntry& b) {
 		return a.nbrPoints > b.nbrPoints;
 	});
-	std::vector<segre::VeldkampLineTableEntry> geometry3_lin_table = geometry3.makeVeldkampLinesTable(vLines3, vPoints3, geometry3_hyp_table);
-	std::sort(geometry3_lin_table.begin(), geometry3_lin_table.end(), [](const segre::VeldkampLineTableEntry& a, const segre::VeldkampLineTableEntry& b){
-		return std::make_tuple(a.isProjective, a.coreNbrPoints, a.coreNbrLines) < std::make_tuple(b.isProjective, b.coreNbrPoints, b.coreNbrLines);
+
+	std::vector<segre::HyperplaneTableEntry> geometry3_hyp_table_with_points_order = geometry3.makeHyperplaneTable<true>(vPoints3);
+	std::sort(geometry3_hyp_table_with_points_order.begin(), geometry3_hyp_table_with_points_order.end(), [] (const segre::HyperplaneTableEntry& a, const segre::HyperplaneTableEntry& b) {
+		return a.nbrPoints > b.nbrPoints;
 	});
 
-	std::vector<segre::VeldkampLineTableEntryWithLines<PPL>> geometry3_lin_table_with_lines = geometry3.makeVeldkampLinesTableWithLines(vLines3, vPoints3, geometry3_hyp_table);
-	std::sort(geometry3_lin_table_with_lines.begin(), geometry3_lin_table_with_lines.end(), [](const segre::VeldkampLineTableEntryWithLines<PPL>& a, const segre::VeldkampLineTableEntryWithLines<PPL>& b){
-		return std::make_tuple(a.entry.isProjective, a.entry.coreNbrPoints, a.entry.coreNbrLines) < std::make_tuple(b.entry.isProjective, b.entry.coreNbrPoints, b.entry.coreNbrLines);
-	});
-
-	//std::vector<std::vector<unsigned int>> permutations_table = segre::makePermutationsTable<3, PPL>(vPoints3);
-	//std::vector<segre::VeldkampLineTableEntry> geometry3_lin_table_sep = segre::separateByPermutations<3,PPL>(geometry3_lin_table_with_lines, permutations_table);
-
-	/*std::vector<std::vector<unsigned int>> coord_permutation_table = segre::makeCoordPermutationsTable<3, PPL>(vPoints3);
-	std::vector<std::vector<unsigned int>> dimension_permutation_table = segre::makeDimensionPermutationsTable<3, PPL>(vPoints3);
-	std::vector<segre::VeldkampLineTableEntry> geometry3_lin_table_sep_2steps = segre::separateBy2StepsPermutations<3,PPL>(geometry3_lin_table_with_lines, coord_permutation_table, dimension_permutation_table);
-
-	VPoints<4> vPoints4 = geometry3.computeHyperplanesFromVeldkampLines(vPoints3, vLines3.projectives);
-	std::vector<segre::HyperplaneTableEntry> geometry4_hyp_table = geometry4.makeHyperplaneTable<COMPUTE_AND_PRINT_POINTS_ORDER>(vPoints4, geometry3_hyp_table);
-
-	std::sort(geometry4_hyp_table.begin(), geometry4_hyp_table.end(), [] (const segre::HyperplaneTableEntry& a, const segre::HyperplaneTableEntry& b) {
+	/*std::vector<segre::HyperplaneTableEntry> geometry3_hyp_table_with_subgeometries = geometry3.makeHyperplaneTable<COMPUTE_AND_PRINT_POINTS_ORDER>(vPoints3, geometry2_hyp_table);
+	std::sort(geometry3_hyp_table_with_subgeometries.begin(), geometry3_hyp_table_with_subgeometries.end(), [] (const segre::HyperplaneTableEntry& a, const segre::HyperplaneTableEntry& b) {
 		return a.nbrPoints > b.nbrPoints;
 	});*/
 
@@ -88,26 +112,14 @@ int main() {
 	std::cout << "\nDimension 2 points:\n";
 	std::copy(geometry2_hyp_table.begin(), geometry2_hyp_table.end(), std::ostream_iterator<segre::HyperplaneTableEntry>(std::cout, "\n"));
 
-	std::cout << "\nDimension 2 lines:\n";
-	std::copy(geometry2_lin_table.begin(), geometry2_lin_table.end(), std::ostream_iterator<segre::VeldkampLineTableEntry>(std::cout, "\n"));
-
 	std::cout << "\nDimension 3 points:\n";
 	std::copy(geometry3_hyp_table.begin(), geometry3_hyp_table.end(), std::ostream_iterator<segre::HyperplaneTableEntry>(std::cout, "\n"));
 
-	std::cout << "\nDimension 3 lines:\n";
-	std::copy(geometry3_lin_table.begin(), geometry3_lin_table.end(), std::ostream_iterator<segre::VeldkampLineTableEntry>(std::cout, "\n"));
-
-	/*std::cout << "\nDimension 4 points:\n";
-	std::copy(geometry4_hyp_table.begin(), geometry4_hyp_table.end(), std::ostream_iterator<segre::HyperplaneTableEntry>(std::cout, "\n"));*/
-
 	LatexPrinter printer;
 	printer.generateHyperplanesTable<COMPUTE_AND_PRINT_POINTS_ORDER,PRINT_SUBGEOMETRIES>(2, geometry2_hyp_table, 0);
-	printer.generateLinesTable(2, geometry2_lin_table, geometry2_hyp_table.size());
-	printer.generateHyperplanesTable<COMPUTE_AND_PRINT_POINTS_ORDER,PRINT_SUBGEOMETRIES>(3, geometry3_hyp_table, geometry2_hyp_table.size());
-	printer.generateLinesTable(3, geometry3_lin_table, geometry3_hyp_table.size());
-	//printer.generateLinesDiffTable(3, geometry3_lin_table, geometry3_lin_table_sep, geometry3_hyp_table.size());
-	/*printer.generateLinesDiffTable(3, geometry3_lin_table, geometry3_lin_table_sep_2steps, geometry3_hyp_table.size());
-	printer.generateHyperplanesTable<COMPUTE_AND_PRINT_POINTS_ORDER,PRINT_SUBGEOMETRIES>(4, geometry4_hyp_table, geometry3_hyp_table.size());*/
+	printer.generateHyperplanesTable<false,false>(3, geometry3_hyp_table, geometry2_hyp_table.size());
+	printer.generateHyperplanesDiffTable<true, false>(3, geometry3_hyp_table, geometry3_hyp_table_with_points_order, geometry2_hyp_table.size());
+	//printer.generateHyperplanesDiffTable<COMPUTE_AND_PRINT_POINTS_ORDER, true>(3, geometry3_hyp_table, geometry3_hyp_table_with_subgeometries, geometry2_hyp_table.size());
 
 	return EXIT_SUCCESS;
 }
